196b73ff45b363d6a316d72fe93a7cbb2352c6a2
jjustinwhite <Jjustinwhite@mac.com>
2016-08-17 20:54:56 -0500

    add return date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,15 @@
 class FlightsController < ApplicationController
 
   def index
+
+    current_month = Time.now.strftime "%Y-%m"
+
     if params[:q] && !params[:q].empty?
       results = Skyscanner::Connection.browse_quotes({ :country => "US",
         :currency => "USD", :locale => "en-US", :originPlace => params[:q],
-        :destinationPlace => "anywhere", :outboundPartialDate => "2016-08",
-        :inboundPartialDate => "2016-08" })
+        :destinationPlace => "US", :outboundPartialDate => current_month,
+        :inboundPartialDate => current_month })
+
 
       @quotes = results["Quotes"].sort_by {|price| price["MinPrice"]}
       @places = results["Places"]
